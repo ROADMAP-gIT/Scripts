3f4ec7f8cab5c03f742c170c86b3bd5779ca9815
ROADMAP-gIT <38221642+ROADMAP-gIT@users.noreply.github.com>
2025-03-17 13:37:02 +0000

    Create P81.sh

---
+++ P81.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+if [ -d /Applications/Perimeter\ 81.app ]; 
+	then echo "Perimeter 81 already Installed, exiting!"
+else
+	pkgURL=$(curl -sL https://support.perimeter81.com/docs/downloading-the-agent | grep -o 'Harmony[^"]*.pkg')
+	downloadURL="https://static.perimeter81.com/agents/mac/$pkgURL"
+	curl -L -o /Users/Shared/HarmonySase.pkg $downloadURL 
+	sudo installer -pkg /Users/Shared/HarmonySase.pkg -target /
+	rm -r /Users/Shared/HarmonySase.pkg
